df4200a38dba372ed351f059d547052506103be9
sMinori <minori.shimizu915@gmail.com>
2023-05-15 22:56:52 +0900

    mybatisを使った実装に修正

--- src/main/java/com/example/mybatisdemo/mapper/MovieMapper.java
+++ src/main/java/com/example/mybatisdemo/mapper/MovieMapper.java
@@ -10,11 +10,14 @@ import java.util.Optional;
 
 @Mapper
 public interface MovieMapper {
-    @Select("SELECT * FROM movies")
+    
     List<Movie> findAll();
 
+    /*@Select("SELECT * FROM movies WHERE movie_id = #{id}")
+    Optional<Movie> findById(int id);*/
+
     @Select("SELECT * FROM movies WHERE movie_id = #{id}")
-    Optional<Movie> findById(int id);
+    Movie findById(Integer id);
 
     @Insert("INSERT INTO movies (name, director) VALUES (#{name}, #{director})")
     void create(Movie movie);
